cdad6ad6d5f2785a6659c3fe9f4445fe508073db
Tiko8686 <tigran.aghajanyan.8686@gmail.com>
2022-04-18 18:12:51 +0400

    test pr

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Historical from "../container/Historical";
 
 import Home from "../container/Home/Home";
+import Masnagitakan from "../container/Masnagitakan";
 import Menu from "./Menu";
 
 function MyRoute() {
@@ -11,12 +13,11 @@ function MyRoute() {
     <Menu></Menu>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/category/historical" element={<Home />} />
-          <Route path="/category/artistic" element={<Home />} />
-          <Route path="/category/historical "element={<Home />} />
+          <Route path="/category/professional" element={<Historical/>} />
+          <Route path="/category/artistic" element={<Historical/>} />
+          <Route path="/category/historical "element={<Masnagitakan />} />
           <Route path="/category/motivational" element={<Home />} />
         </Routes>
-  
       </BrowserRouter>
     </>
   );
---
+++ src/container/Historical.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Historical() {
+  return (
+    <div>
+      <h1>Historical</h1>
+    </div>
+  );
+}
+
+export default Historical;
