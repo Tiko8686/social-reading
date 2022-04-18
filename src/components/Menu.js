a3f675506acf5839350f25605cb834e958ccf622
yelenaMkrtchyann <101735829+yelenaMkrtchyann@users.noreply.github.com>
2022-04-18 22:50:16 +0400

    menu
    menun poxel em Bootstrapov

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,35 @@
 import { Link } from "react-router-dom";
-
+import "./menu.css"
 function Menu() {
   return (
-    <>
-    <div>
-        <h1> Kategorianer</h1>
-      <nav>
-        <Link to="/category/professional">Մասնագիտական</Link><br></br>
-        <Link to="/category/artistic">Գեղարվեստական</Link><br></br>
-        <Link to="/category/historical">Պատմական</Link><br></br>
-        <Link to="/category/motivational">Մոտիվացիոն</Link><br></br>
+    <header class="section-header py-4">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="main_nav">
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                <span class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Hover me</span>
+                <ul class="dropdown-menu fade-up">
+                  <li>
+                    <Link class="dropdown-item" to="/category/professional">Մասնագիտական</Link>
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="/category/artistic">Գեղարվեստական</Link>
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="/category/historical">Պատմական</Link>
+                  </li>
+                  <li>
+                    <Link class="dropdown-item" to="/category/motivational">Մոտիվացիոն</Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
-      </div>
-    </>
+    </header>
+
   );
 }
 export default Menu;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "../container/Home/Home";
 import Menu from "./Menu";
