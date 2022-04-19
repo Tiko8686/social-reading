42afb8ff2c376d2bbd523be022a2fbbf63f3ad34
Tiko8686 <tigran.aghajanyan.8686@gmail.com>
2022-04-19 15:01:53 +0400

    asas
    vbgc

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,31 +1,35 @@
 import { Link } from "react-router-dom";
-import "./menu.css"
+
 function Menu() {
   return (
     <>
-      <div>
-        <h1> Կատեգորիաներ</h1>
-        <nav>
+      <div className="dropdown">
+        <h2> Կատեգորիաներ</h2>
+       
+        <nav className="dropdown-content">
           <li>
             <Link to="/category/professional">Մասնագիտական</Link>
-          </li>{" "}
+          </li>
           <br></br>
           <li>
             <Link to="/category/artistic">Գեղարվեստական</Link>
           </li>
           <br></br>
           <li>
-           
             <Link to="/category/historical">Պատմական</Link>
           </li>
           <br></br>
           <li>
-            {" "}
+        
             <Link to="/category/motivational">Մոտիվացիոն</Link>
           </li>
           <br></br>
+         <li> <Link to="/category/hogebanakan">Հոգեբանական</Link></li>
+          
         </nav>
+
       </div>
+      <button className="myBtn">UPLOAD</button> 
     </>
   );
 }
