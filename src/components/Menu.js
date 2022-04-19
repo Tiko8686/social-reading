b3f88647ad24ad007238641d9228626d401c1262
yelenaMkrtchyann <101735829+yelenaMkrtchyann@users.noreply.github.com>
2022-04-19 15:28:13 +0400

    menu style

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,38 +1,41 @@
 import { Link } from "react-router-dom";
-
-
+import "./menu.css"
 function Menu() {
   return (
+    <nav>
 
-    <>
-      <div className="dropdown">
-        <h2> Կատեգորիաներ</h2>
-       
-        <nav className="dropdown-content">
-          <li>
-            <Link to="/category/professional">Մասնագիտական</Link>
-          </li>
-          <br></br>
-          <li>
-            <Link to="/category/artistic">Գեղարվեստական</Link>
-          </li>
-          <br></br>
-          <li>
-            <Link to="/category/historical">Պատմական</Link>
-          </li>
-          <br></br>
-          <li>
-        
-            <Link to="/category/motivational">Մոտիվացիոն</Link>
-          </li>
-          <br></br>
-         <li> <Link to="/category/hogebanakan">Հոգեբանական</Link></li>
-          
-        </nav>
+      <ul>
+        <li>
+          <Link to="/">Գլխավոր</Link>
+        </li>
+        <li className="dropdown">
+          <span>Կատեգորիաներ</span>
 
-      </div>
-      <button className="myBtn">UPLOAD</button> 
-    </>
+          <ul className="dropdown-content">
+            <li>
+              <Link to="/category/professional">Մասնագիտական</Link>
+            </li>
+            <br></br>
+            <li>
+              <Link to="/category/artistic">Գեղարվեստական</Link>
+            </li>
+            <br></br>
+            <li>
+              <Link to="/category/historical">Պատմական</Link>
+            </li>
+            <br></br>
+            <li>
+              <Link to="/category/motivational">Մոտիվացիոն</Link>
+            </li>
+            <br></br>
+            <li> <Link to="/category/hogebanakan">Հոգեբանական</Link></li>
+          </ul>
 
-  )}
+        </li>
+
+      </ul>
+      <button className="myBtn">UPLOAD</button>
+    </nav>
+  )
+}
 export default Menu;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,4 @@
-
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Historical from "../container/Historical";
 
 
