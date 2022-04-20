f418ef4bed949e9895d90cdeb879cadd7809ac02
yelenaMkrtchyann <101735829+yelenaMkrtchyann@users.noreply.github.com>
2022-04-20 14:09:53 +0400

    aa

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,39 +3,50 @@ import { Modal } from "./Upload";
 import "./menu.css";
 function Menu() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/" >Գլխավոր</Link>
-        </li>
-        <li className="dropdown">
-          <span>Կատեգորիաներ</span>
 
-          <ul className="dropdown-content">
-            <li>
-              <Link to="/category/professional">Մասնագիտական</Link>
-            </li>
-            <br></br>
-            <li>
-              <Link to="/category/artistic">Գեղարվեստական</Link>
-            </li>
-            <br></br>
-            <li>
-              <Link to="/category/historical">Պատմական</Link>
-            </li>
-            <br></br>
-            <li>
-              <Link to="/category/motivational">Մոտիվացիոն</Link>
-            </li>
-            <br></br>
-            <li>
-              <Link to="/category/hogebanakan">Հոգեբանական</Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <Modal />
-    </nav>
+    <nav>
+    <ul>
+      <li>
+        <Link to="/" >Գլխավոր</Link>
+      </li>
+    </ul>
+    <ul >
+      <li className="dropdown">
+        <span>Կատեգորիաներ</span>
+        <ul className="dropdown-content">
+          <li>
+            <Link to="/category/professional">Մասնագիտական</Link>
+          </li>
+          <br></br>
+          <li>
+            <Link to="/category/artistic">Գեղարվեստական</Link>
+          </li>
+          <br></br>
+          <li>
+            <Link to="/category/historical">Պատմական</Link>
+          </li>
+          <br></br>
+          <li>
+            <Link to="/category/motivational">Մոտիվացիոն</Link>
+          </li>
+          <br></br>
+          <li> <Link to="/category/psychological">Հոգեբանական</Link></li>
+        </ul>
+      </li>
+      <li>
+        <Link to="#" >Մեր մասին</Link>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <Modal />
+
+      </li>
+      <li>
+        <input placeholder="Որոնել" />
+      </li>
+    </ul>
+  </nav>
   );
 }
 export default Menu;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Artistic from "../container/Artistic";
-import Historical from "../container/Historical";
-import Home from "../container/Home";
-import Professional from "../container/Professional";
-import Psychological from "../container/Psychological";
+import Artistic from "../container/Artistic/Artistic";
+import Historical from "../container/Historical/Historical";
+import Home from "../container/Home/Home";
+import Professional from "../container/Professional/Professional";
+import Psychological from "../container/Psychological/Psychological";
+import Motivation from "../container/Motivation/Motivation";
+
 import Menu from "./Menu";
 
 function AllRoutes() {
@@ -16,7 +18,7 @@ function AllRoutes() {
           <Route path="/category/professional" element={<Professional />} />
           <Route path="/category/artistic" element={<Artistic />} />
           <Route path="/category/historical" element={<Historical />} />
-          <Route path="/category/motivational" element={<Motivational/>} />
+          <Route path="/category/motivational" element={<Motivation/>} />
           <Route path="/category/psychological" element={<Psychological />} />
         </Routes>
       </BrowserRouter>
