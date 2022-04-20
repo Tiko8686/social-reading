6160e0e351315b5ba1918772f6ded363ca44d586
yelenaMkrtchyann <101735829+yelenaMkrtchyann@users.noreply.github.com>
2022-04-20 14:15:33 +0400

    style fixed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,52 +1,51 @@
 import { Link } from "react-router-dom";
-import { Modal } from "./Upload";
-import "./menu.css";
+import { Modal } from "../components/Upload";
+import "./menu.css"
 function Menu() {
   return (
-
     <nav>
-    <ul>
-      <li>
-        <Link to="/" >Գլխավոր</Link>
-      </li>
-    </ul>
-    <ul >
-      <li className="dropdown">
-        <span>Կատեգորիաներ</span>
-        <ul className="dropdown-content">
-          <li>
-            <Link to="/category/professional">Մասնագիտական</Link>
-          </li>
-          <br></br>
-          <li>
-            <Link to="/category/artistic">Գեղարվեստական</Link>
-          </li>
-          <br></br>
-          <li>
-            <Link to="/category/historical">Պատմական</Link>
-          </li>
-          <br></br>
-          <li>
-            <Link to="/category/motivational">Մոտիվացիոն</Link>
-          </li>
-          <br></br>
-          <li> <Link to="/category/psychological">Հոգեբանական</Link></li>
-        </ul>
-      </li>
-      <li>
-        <Link to="#" >Մեր մասին</Link>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <Modal />
+      <ul>
+        <li>
+          <Link to="/" >Գլխավոր</Link>
+        </li>
+      </ul>
+      <ul >
+        <li className="dropdown">
+          <span>Կատեգորիաներ</span>
+          <ul className="dropdown-content">
+            <li>
+              <Link to="/category/professional">Մասնագիտական</Link>
+            </li>
+            <br></br>
+            <li>
+              <Link to="/category/artistic">Գեղարվեստական</Link>
+            </li>
+            <br></br>
+            <li>
+              <Link to="/category/historical">Պատմական</Link>
+            </li>
+            <br></br>
+            <li>
+              <Link to="/category/motivational">Մոտիվացիոն</Link>
+            </li>
+            <br></br>
+            <li> <Link to="/category/psychological">Հոգեբանական</Link></li>
+          </ul>
+        </li>
+        <li>
+          <Link to="#" >Մեր մասին</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Modal />
 
-      </li>
-      <li>
-        <input placeholder="Որոնել" />
-      </li>
-    </ul>
-  </nav>
-  );
+        </li>
+        <li>
+          <input placeholder="Որոնել" />
+        </li>
+      </ul>
+    </nav>
+  )
 }
 export default Menu;
