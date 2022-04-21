25550ddb8028d476d2a6a9d8bb471a15ee320776
sardaryanvah <sardaryan.vah2004@gmail.com>
2022-04-21 03:23:30 -0700

    sw

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import "./Modal.css";
 
 export function Modal() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const [modal, setModal] = useState(false);
   const toggleModal = () => {
     setModal(!modal);
@@ -19,6 +19,9 @@ export function Modal() {
     fetch("http://192.168.0.124:8000/quotes/", requestOptions).then((res) =>
       console.log(res)
     );
+    toggleModal();
+    reset({ bookName: '' })
+    reset({ image: '' })
   };
   return (
     <>
@@ -29,19 +32,31 @@ export function Modal() {
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2>Upload Something</h2>
+            <button className="close" onClick={toggleModal}>X</button>
             <form onSubmit={handleSubmit(onSubmit)}>
+              <label for="name" className="bookNameLabel">
+                Գրքի անուն
+              </label>
               <input
+                className="BookNameInput"
+                id="name"
                 type="text"
-                placeholder="Name of book"
                 {...register("bookName")}
               />
+              <label for="files" className="fileLabel">
+                Հատված գրքից
+              </label>
               <input
                 type="file"
+                id="files"
                 accept="image/png,image/jpeg"
+                class="fileInput"
                 {...register("image")}
               />
-              <input className="submit" type="submit" />
+              <label for="files" className="fileButton">
+                <span>Ընտրել նկար</span>
+              </label>
+              <input className="submit" type="submit" value="Վերբեռնել" />
             </form>
           </div>
         </div>
