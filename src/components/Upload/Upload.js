dcf5d1f40c4ae44da44e4ad1a1521486fdb83085
sardaryanvah <sardaryan.vah2004@gmail.com>
2022-04-27 23:25:58 -0700

    Update Upload.js

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -6,28 +6,15 @@ import "./Upload.css";
 export function Modal() {
   const { register, handleSubmit, reset } = useForm();
   const [modal, setModal] = useState(false);
-  const [baseImage, setBaseImage] = useState("");
 
-  const uploadImage = async (e) => {
-    const file = await e.target.files[0];
-    const base64 = await convertBase64(file);
-    await setBaseImage(base64);
-    console.log(baseImage);
-  };
-  const convertBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(file);
+  const createURL = (event) =>{
+    if (event.target.files.length > 0) {
+      let file = event.target.files[0];
+      let src =URL.createObjectURL(file);
 
-      fileReader.onload = () => {
-        resolve(fileReader.result);
-      };
-
-      fileReader.onerror = (error) => {
-        reject(error);
-      };
-    });
-  };
+      console.log(src);
+    }
+  }
 
   const toggleModal = () => {
     setModal(!modal);
@@ -69,8 +56,7 @@ export function Modal() {
               X
             </button>
             <div className="imageDiv">
-              {baseImage &&
-                <img src={baseImage} className="img" />}
+                <img className="img" />
               <label for="files" className="fileLabel bi bi-cloud-upload">
                 Վերբեռնել Նկար
               </label>
@@ -99,9 +85,7 @@ export function Modal() {
               />
               
               <input
-                onChange={(e) => {
-                  uploadImage(e);
-                }}
+                onClick={createURL}
                 type="file"
                 id="files"
                 accept="image/png,image/jpeg, image/jpg"
