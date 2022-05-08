7926a85a63f22ad18730b6d87d2ee21b3cfbf060
Tiko8686 <tigran.aghajanyan.8686@gmail.com>
2022-05-09 00:20:15 +0400

    r

--- src/components/Signin/googleLogin.js
+++ src/components/Signin/googleLogin.js
@@ -2,35 +2,34 @@ import GoogleLogin from "react-google-login";
 
 const clientId =
   "699412387887-1pae7suk9l9bstfdaer6826gjc118k0m.apps.googleusercontent.com";
-  function useAuth0() {
+  // function useAuth0() {
  
-        gravatar: string;
-        getAccessToken: () => Promise<void>;
-        accessToken: string;
-      }
-    
- 
-    
-      const [accessToken, setAccessToken] = useState("");
+  //       gravatar: string;
+  //       getAccessToken: () => Promise<void>;
+  //       accessToken: string;
+  //     }
     
  
     
-      async function getAccessToken(): Promise<void> {
-        try {
-          const token = await auth0Client?.getTokenSilently();
-          setAccessToken(token);
-        } catch (e) {
-          console.log(e);
-        }
-      }
+     // const [accessToken, setAccessToken] = useState("");
     
-    ...
+ 
     
-      return {
-    ...
-        accessToken,
-        getAccessToken,
-      };
+    //   async function getAccessToken(): Promise<void> {
+    //     try {
+    //       const token = await auth0Client?.getTokenSilently();
+    //       setAccessToken(token);
+    //     } catch (e) {
+    //       console.log(e);
+    //     }
+    //   }
+    
+    // ...
+    
+    //   return {
+    //  accessToken,
+    //     getAccessToken,
+  //    };
 function Login() {
     
   const onSuccess = (res) => {
