0cfae490078c3067c2761d7d472ad53d01e881d5
AregArpify <104068665+AregArpify@users.noreply.github.com>
2022-05-17 14:41:21 +0400

    Posts

--- src/container/Post/Post.js
+++ src/container/Post/Post.js
@@ -1,9 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./post.css"
 function Post() {
+  const [userPosts, setUserPosts] = useState("")
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("user"))
+    axios.get("https://www.socialreading.xyz/quotes/?author_id=" + userInfo.id).then((resp) => {
+      setUserPosts(resp);
+    })
+  }, [])
+
   return (
     <>
       <div className="profile_post">
-        <h1>Post</h1>
+        {userPosts?.data?.map((post) => (
+          <div className="post">
+            <img src={post.quote_file} className="post_img"/>
+          </div>
+        ))}
       </div>
     </>
 
--- src/container/Profile/Profile.js
+++ src/container/Profile/Profile.js
@@ -17,16 +17,21 @@ function Profile() {
         console.log(userInfo);
     }, [])
     const [modal, setModal] = useState(false)
-    return (<div className="profilePage" >
-        <div className="profile_background" >
-        </div>
-        <div className="my_info" >
-            <div className="name_and_pic">
-                <div className="pic_div" >
-                    <img className="autor_pic"  src={userInfo.avatar}/>
+    return (<>
+        <div className="profilePage" >
+            <div className="profile_background" >
+            </div>
+            <div className="my_info" >
+                <div className="name_and_pic">
+                    <div className="pic_div" >
+                        <img className="autor_pic" src={userInfo.avatar} />
+                    </div>
+                    <div className="nameSurnameDiv">
+                        <h3> {userInfo.first_name + " " + userInfo.last_name}  </h3>
+                    </div>
                 </div>
-                <div className="nameSurnameDiv">
-                    <h3> {userInfo.first_name + " " + userInfo.last_name}  </h3>
+                <div>
+                    <button className="edit_btn bi bi-pencil">&nbsp; Edit profile</button>
                 </div>
             </div>
             <div className="profile_menu" >
@@ -46,33 +51,37 @@ function Profile() {
                     {modal &&
                         <ul className="modal_menu_profile" onClick={() => setModal(false)}>
                             <li>
-                                <Link to="/settings">Settings</Link>
+                                <Link to="/settings" className="bi bi-gear settings">&nbsp; Settings</Link>
                             </li>
                             <li>
-                                <Link to="/privacePolicy">Privace Policy</Link>
+                                <img src="http://localhost:3000/images/privace_policy.png" className="privace_poilcy" />
+                                <Link to="/privacePolicy" className="privace_poilcy_txt">
+                                &nbsp; Privace Policy</Link>
                             </li>
                             <li>
+                                <img src="http://localhost:3000/images/LogOut.png" className="log_out_img" />
                                 <button onClick={() => {
                                     localStorage.removeItem("token")
                                     localStorage.removeItem("user")
-
                                     localStorage.removeItem("tokenGoogle")
                                     localStorage.removeItem("userGoogle")
-
                                     localStorage.removeItem("tokenFb")
                                     localStorage.removeItem("userFb")
                                     navigate("/")
                                     window.location.reload()
-                                }}>Log Out</button>
+                                }}
+                                    className="log_out">
+                                    &nbsp; Log Out
+                                </button>
                             </li>
                         </ul>
                     }
                 </ul>
             </div>
+
         </div>
         <Outlet />
     </>
-
     );
 }
 
