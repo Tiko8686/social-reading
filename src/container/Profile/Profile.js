4849161218e07cadb5a2f3a73fb8deaa7b4e3ee9
AregArpify <104068665+AregArpify@users.noreply.github.com>
2022-05-20 14:30:02 +0400

    change profile pic

--- src/container/Profile/Profile.js
+++ src/container/Profile/Profile.js
@@ -1,27 +1,76 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import "./profile.css";
+import { Button, Box, Slider } from "@material-ui/core";
+import AvatarEditor from "react-avatar-editor";
 
 function Profile() {
     const [userInfo, setUserInfo] = useState("");
     const [userGoogle, setUserGoogle] = useState("");
     const [userFb, setUserFb] = useState("");
-
     const navigate = useNavigate();
+    let editor = "";
+
     useEffect(() => {
-        console.log("aaaa")
-        setUserInfo(JSON.parse(localStorage.getItem("user")));
         const token = JSON.parse(localStorage.getItem("token"));
         const tokenGoogle = JSON.parse(localStorage.getItem("tokenGoogle"));
         const tokenFb = JSON.parse(localStorage.getItem("tokenFb"));
-        setUserGoogle(JSON.parse(localStorage.getItem("userGoogle")));
-        setUserFb(JSON.parse(localStorage.getItem("userFb")));
+
         if (!token && !tokenGoogle && !tokenFb) {
             navigate("/");
+        } else {
+            if (JSON.parse(localStorage.getItem("user"))) {
+                setUserInfo(JSON.parse(localStorage.getItem("user")));
+                setPicture({ ...picture, croppedImg: JSON.parse(localStorage.getItem("user")).avatar })
+            } else if (JSON.parse(localStorage.getItem("userGoogle"))) {
+                setUserGoogle(JSON.parse(localStorage.getItem("userGoogle")));
+                setPicture({ ...picture, croppedImg: JSON.parse(localStorage.getItem("userGoogle")).avatar_google })
+            } else if (JSON.parse(localStorage.getItem("userFb"))) {
+                setUserFb(JSON.parse(localStorage.getItem("userFb")));
+                setPicture({ ...picture, croppedImg: JSON.parse(localStorage.getItem("userFb")).avatar_facebook })
+            }
         }
     }, []);
+    const [picture, setPicture] = useState({
+        cropperOpen: false,
+        img: null,
+        zoom: 2,
+        croppedImg: ""
+    });
 
     const [modal, setModal] = useState(false);
+
+    const handleSlider = (event, value) => {
+        setPicture({
+            ...picture,
+            zoom: value
+        });
+    };
+
+    const handleCancel = () => {
+        setPicture({
+            ...picture,
+            cropperOpen: false
+        });
+    };
+
+    const setEditorRef = (ed) => {
+        editor = ed;
+    };
+
+    const handleSave = (e) => {
+        if (setEditorRef) {
+            const canvasScaled = editor.getImageScaledToCanvas();
+            const croppedImg = canvasScaled.toDataURL();
+            console.log("croppedImg", croppedImg)
+            setPicture({
+                ...picture,
+                img: null,
+                cropperOpen: false,
+                croppedImg: croppedImg
+            });
+        }
+    };
     return (
         <>
             <div className="profilePage">
@@ -48,35 +97,30 @@ function Profile() {
                             )
                                 : ""
                     }
-                    <label for="cover_photo" className="bi bi-camera edit_cover_photo_button">Add cover Photo</label>
-                    <input type="file" id="cover_photo" />
+                    <label htmlFor="cover_photo" className="bi bi-camera edit_cover_photo_button">Add cover Photo</label>
+                    <input type="file" id="cover_photo" onChange={(e) => {
+
+                    }}/>
                 </div>
                 <div className="my_info">
                     <div className="name_and_pic">
                         <div className="pic_div">
-                            {userInfo ? (
-                                <img
-                                    className="autor_pic"
-                                    src={userInfo.avatar}
-                                    alt="profile_pic"
-                                />
-                            ) : userGoogle ? (
-                                <img
-                                    className="autor_pic"
-                                    src={userGoogle?.avatar_google}
-                                    alt="profile_pic"
-                                />
-                            ) : userFb ? (
-                                <img
-                                    className="autor_pic"
-                                    src={userFb.avatar_facebook}
-                                    alt="profile_pic"
-                                />
-                            ) : (
-                                ""
-                            )}
-                            <label for="profile_photo" className="bi bi-camera edit_profile_pic"></label>
-                            <input type="file" id="profile_photo" />
+                            <img
+                                className="autor_pic"
+                                src={picture.croppedImg}
+                                alt="profile_pic"
+                            />
+                            <label htmlFor="profile_photo" className="bi bi-camera edit_profile_pic"></label>
+                            <input type="file" id="profile_photo" onChange={(e) => {
+                                let url = URL.createObjectURL(e.target.files[0]);
+                                console.log(url);
+                                setPicture({
+                                    ...picture,
+                                    img: url,
+                                    cropperOpen: true
+                                });
+                            }} />
+
                         </div>
                         <div className="nameSurnameDiv">
                             <h3>
@@ -158,6 +202,38 @@ function Profile() {
                     </ul>
                 </div>
             </div>
+
+            {picture.cropperOpen && (
+                <div className="overlay" >
+                    <Box display="block" className="image_crop">
+                        <AvatarEditor
+                            ref={setEditorRef}
+                            image={picture.img}
+                            width={200}
+                            height={200}
+                            border={50}
+                            color={[255, 255, 255, 0.6]}
+                            rotate={0}
+                            scale={picture.zoom}
+                        />
+                        <Slider
+                            aria-label="raceSlider"
+                            value={picture.zoom}
+                            min={1}
+                            max={10}
+                            step={0.1}
+                            onChange={handleSlider}
+                        ></Slider>
+                        <Box>
+                            <Button variant="contained" onClick={handleCancel}>
+                                Cancel
+                            </Button>
+                            <Button onClick={handleSave}>Save</Button>
+                        </Box>
+                    </Box>
+                </div>
+
+            )}
             <Outlet />
         </>
     );
